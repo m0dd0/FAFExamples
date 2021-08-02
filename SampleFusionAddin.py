26057442764e35877120f670cbe36c56eaadb7c3
Moritz <mo.hesche@gmail.com>
2021-08-02 17:25:53 +0200

    module logger

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -2,20 +2,37 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 from .fusion_addin_framework import fusion_addin_framework as faf
 
+import logging
+
+addin = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
 
 def run(context):
-    try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    logger = logging.getLogger(faf.__name__)
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    logger.addHandler(stream_handler)
+    palette_handler = faf.utils.TextPaletteLoggingHandler()
+    logger.addHandler(palette_handler)
+
+    # alternativly you can use this utiltiy function
+    # faf.utils.create_logger(
+    #     faf.__name__,
+    #     [logging.StreamHandler(), faf.utils.TextPaletteLoggingHandler()],
+    # )
+
+    global addin
+    addin = faf.FusionAddin()
+    ws = faf.Workspace(parent=addin, id="FusionSolidEnvironment")
+    tab = faf.Tab(parent=ws, id="SolidTab")
+    panel = faf.Panel(parent=tab, id="SolidCreatePanel")
+    control = faf.Control(parent=panel, isPromoted=True)
+    cmd = faf.AddinCommand(parent=control, onExecute=say_hi, name="my command")
 
 
 def stop(context):
-    try:
-        pass
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    addin.stop()
