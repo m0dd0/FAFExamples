e5af2e98509c08d731a2ba2c1c3a1e8e9bf4eb49
Moritz <mo.hesche@gmail.com>
2021-08-02 14:34:36 +0200

    very custom position

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -3,19 +3,31 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 from .fusion_addin_framework import fusion_addin_framework as faf
 
 
+# Addin at a very custom position
+addin = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
+
 def run(context):
     try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
+        global addin
+        addin = faf.FusionAddin()
+        # its not possible to create a custom workspace so the Design Workspace is used
+        ws = faf.Workspace(parent=addin, id="FusionSolidEnvironment")
+        # passing the "random" as an id will generate an UUID, it would be also possible
+        # to use a custom id like "MySuperCustomId1234"
+        tab = faf.Tab(parent=ws, id="random", name="my tab")
+        panel = faf.Panel(parent=tab, id="random", name="my panel")
+        control = faf.Control(parent=panel, isPromoted=True, isPromotedByDefault=True)
+        cmd = faf.AddinCommand(
+            parent=control, onExecute=say_hi, name="my command", resourceFolder="cubes"
         )
+    except:
+        print("except")
 
 
 def stop(context):
-    try:
-        pass
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    addin.stop()
