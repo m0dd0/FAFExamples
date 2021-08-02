d5c61bde6cce41b9f894e3bf9d997322e628fffa
Moritz <mo.hesche@gmail.com>
2021-08-02 14:32:07 +0200

    specify_posittion_dotted

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -3,19 +3,25 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 from .fusion_addin_framework import fusion_addin_framework as faf
 
 
+# specify position of addin dotted style
+cmd = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
+
 def run(context):
-    try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    global cmd
+    cmd = (
+        faf.FusionAddin()
+        .workspace(id="FusionSolidEnvironment")
+        .tab(id="SolidTab")
+        .panel(id="SolidCreatePanel")
+        .control(isPromoted=True)
+        .addinCommand(onExecute=say_hi, name="my command")
+    )
 
 
 def stop(context):
-    try:
-        pass
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    cmd.addin.stop()
