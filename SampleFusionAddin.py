b5d37be46caffca9a12b194500c60937a5f555b7
Moritz <mo.hesche@gmail.com>
2021-08-02 16:12:39 +0200

    checkbo controlled addin

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -2,10 +2,23 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 from .fusion_addin_framework import fusion_addin_framework as faf
 
+addin = None
+
+
+def say_hi(even_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
 
 def run(context):
     try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
+        global addin
+        addin = faf.FusionAddin()
+        workspace = faf.Workspace(addin)
+        tab = faf.Tab(workspace, id="ToolsTab")
+        panel = faf.Panel(tab, id="SolidScriptsAddinsPanel")
+        # use a checkbox instead of a button
+        control = faf.Control(panel, controlType="checkbox")
+        cmd = faf.AddinCommand(control, name="my checkbox command", execute=say_hi)
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
@@ -14,7 +27,7 @@ def run(context):
 
 def stop(context):
     try:
-        pass
+        addin.stop()
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
