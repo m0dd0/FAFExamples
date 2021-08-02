c6c939bfa0e735df9661ac9dc4522f78d1966e99
Moritz <mo.hesche@gmail.com>
2021-08-02 14:12:20 +0200

    basic addin structure

---
+++ SampleFusionAddin.py
@@ -0,0 +1,21 @@
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+# from .fusion_addin_framework import fusion_addin_framework as faf
+
+
+def run(context):
+    try:
+        adsk.core.Application.get().userInterface.messageBox("hi")
+    except:
+        adsk.core.Application.get().userInterface.messageBox(
+            "Failed:\n{}".format(traceback.format_exc())
+        )
+
+
+def stop(context):
+    try:
+        pass
+    except:
+        adsk.core.Application.get().userInterface.messageBox(
+            "Failed:\n{}".format(traceback.format_exc())
+        )
