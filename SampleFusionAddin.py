491496f953f609d80c4fc9d83ead1f353cea743e
Moritz <mo.hesche@gmail.com>
2021-08-02 16:46:27 +0200

    nested dotted dropdown

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -2,10 +2,27 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 from .fusion_addin_framework import fusion_addin_framework as faf
 
+cmd = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
 
 def run(context):
     try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
+        global cmd
+        cmd = (
+            faf.Workspace()
+            .tab()
+            .panel()
+            .dropdown()
+            .dropdown()
+            .dropdown()
+            .dropdown()
+            .control()
+            .addinCommand(execute=say_hi)
+        )
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
@@ -14,7 +31,7 @@ def run(context):
 
 def stop(context):
     try:
-        pass
+        cmd.addin.stop()
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
