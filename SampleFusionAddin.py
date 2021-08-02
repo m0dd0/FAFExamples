8911c4b812e29dddcc225fe578ac495442d17e30
Moritz <mo.hesche@gmail.com>
2021-08-02 16:22:17 +0200

    command with multiple controls

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -2,20 +2,33 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 from .fusion_addin_framework import fusion_addin_framework as faf
 
+addin = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
 
 def run(context):
-    try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    global addin
+    addin = faf.FusionAddin()
+    ws = faf.Workspace(parent=addin, id="FusionSolidEnvironment")
+
+    solid_tab = faf.Tab(parent=ws, id="SolidTab")
+    tools_tab = faf.Tab(parent=ws, id="ToolsTab")
+
+    solid_panel = faf.Panel(parent=solid_tab, id="SolidCreatePanel")
+    addin_panel = faf.Panel(parent=tools_tab, id="SolidScriptsAddinsPanel")
+
+    control_1 = faf.Control(parent=solid_panel, isPromoted=True)
+    control_2 = faf.Control(parent=addin_panel, isPromoted=True)
+
+    # this command has two parental controls and can therfore be acticated from
+    # different postions in the UI
+    cmd = faf.AddinCommand(
+        parent=[control_1, control_2], onExecute=say_hi, name="my command"
+    )
 
 
 def stop(context):
-    try:
-        pass
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    addin.stop()
