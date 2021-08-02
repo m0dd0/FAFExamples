504db3097b0abb201ca9cb0816506291259f95fa
Moritz <mo.hesche@gmail.com>
2021-08-02 15:24:23 +0200

    more handlers

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -3,7 +3,7 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 from .fusion_addin_framework import fusion_addin_framework as faf
 
 
-# Addin with many connected handlers
+# Addin with multiple connected handlers
 cmd = None
 
 
@@ -15,14 +15,32 @@ def say_hi(event_args: adsk.core.CommandEventArgs):
     adsk.core.Application.get().userInterface.messageBox("hi")
 
 
+def say_changed(event_args: adsk.core.InputChangedEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("input changed")
+
+
+def say_by(event_args: adsk.core.CommandCreatedEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("by")
+
+
 def run(context):
     try:
         global cmd
         cmd = faf.AddinCommand(
-            name="my command", onExecute=say_hi, onCommandCreated=create_inputs
+            name="my command",
+            onExecute=say_hi,
+            onCommandCreated=create_inputs,
+            onInputChanged=say_changed,
+            onDestroy=say_by,
         )
-        # it is not necessary to use the "on"-prefix, the line below is equivalent
-        # cmd = faf.AddinCommand(name="my command", execute=say_hi, created=create_inputs )
+        # it is not necessary to use the "on"-prefix, the code below is equivalent
+        # cmd = faf.AddinCommand(
+        #     name="my command",
+        #     execute=say_hi,
+        #     commandCreated=create_inputs,
+        #     inputChanged=say_changed,
+        #     destroy=say_by,
+        # )
 
     except:
         adsk.core.Application.get().userInterface.messageBox(
