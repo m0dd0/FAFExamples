ff1db26863f184f11e2711e6fce39b63e1398442
Moritz <mo.hesche@gmail.com>
2021-08-02 14:30:11 +0200

    specify position

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -3,19 +3,23 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 from .fusion_addin_framework import fusion_addin_framework as faf
 
 
+# specify position of addin
+addin = None
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
+
 def run(context):
-    try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    global addin
+    addin = faf.FusionAddin()
+    ws = faf.Workspace(parent=addin, id="FusionSolidEnvironment")
+    tab = faf.Tab(parent=ws, id="SolidTab")
+    panel = faf.Panel(parent=tab, id="SolidCreatePanel")
+    control = faf.Control(parent=panel, isPromoted=True)
+    cmd = faf.AddinCommand(parent=control, onExecute=say_hi, name="my command")
 
 
 def stop(context):
-    try:
-        pass
-    except:
-        adsk.core.Application.get().userInterface.messageBox(
-            "Failed:\n{}".format(traceback.format_exc())
-        )
+    addin.stop()
