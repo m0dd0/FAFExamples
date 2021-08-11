49f3ccbac7b2b80ddbcf2538919f7e444ea106b0
Moritz <mo.hesche@gmail.com>
2021-08-11 08:53:31 +0200

    demo sample

--- SampleFusionAddin.py
+++ SampleFusionAddin.py
@@ -2,10 +2,42 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 from .fusion_addin_framework import fusion_addin_framework as faf
 
+addin = None
+
+
+def create_inputs(event_args: adsk.core.CommandCreatedEventArgs):
+    event_args.command.commandInputs.addBoolValueInput("boolInputId", "my input", True)
+
+
+def say_hi(event_args: adsk.core.CommandEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("hi")
+
+
+def say_changed(event_args: adsk.core.InputChangedEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("input changed")
+
+
+def say_ending(event_args: adsk.core.CommandCreatedEventArgs):
+    adsk.core.Application.get().userInterface.messageBox("ending")
+
 
 def run(context):
     try:
-        adsk.core.Application.get().userInterface.messageBox("hi")
+        global addin
+        addin = faf.FusionAddin()
+        ws = faf.Workspace(parent=addin, id="FusionSolidEnvironment")
+        tab = faf.Tab(parent=ws, id="SolidTab")
+        panel = faf.Panel(parent=tab, id="SolidCreatePanel")
+        control = faf.Control(parent=panel, isPromoted=True)
+        cmd = faf.AddinCommand(
+            parent=control,
+            name="my command",
+            onExecute=say_hi,
+            onCommandCreated=create_inputs,
+            onInputChanged=say_changed,
+            onDestroy=say_ending,
+        )
+
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
@@ -14,7 +46,7 @@ def run(context):
 
 def stop(context):
     try:
-        pass
+        addin.stop()
     except:
         adsk.core.Application.get().userInterface.messageBox(
             "Failed:\n{}".format(traceback.format_exc())
